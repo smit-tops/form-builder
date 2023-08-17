f2a8342b7456dd170b7a1c7a571a9300bf113a6b
Smit Vadhiya <smit@topsinfosolutions.com>
2023-08-17 14:21:48 +0530

    add all fields components

---
+++ src/lib/components/Fields/Dropdown/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ItemLabel from '../../FormGenrator/ItemLabel'
+import { FormField } from '../../../types/fields'
+import useFieldSet from '../../../hook/useFieldSet'
+import FieldCard from '../../FormGenrator/FieldCard'
+
+export default function DropdownField({
+  field,
+  onChange,
+  provided,
+}: {
+  field: FormField
+  onChange: any
+  provided: any
+}) {
+  const { isEdit, handleLabelChange, handleEdit, handleSave, handleCancel, handleDelete, renderData } = useFieldSet(
+    field,
+    onChange,
+  )
+
+  return (
+    <>
+      <FieldCard
+        handleDelete={() => handleDelete(field.id)}
+        handleEdit={handleEdit}
+        handleCancel={handleCancel}
+        handleSave={handleSave}
+        isEdit={isEdit}
+        value={renderData}
+        provided={provided}
+      >
+        <ItemLabel
+          edit={isEdit}
+          value={renderData.label}
+          onChange={handleLabelChange}
+          className="form-control"
+          label="Question"
+          keyField="label"
+        />
+        DROPDOWN OPTIONS
+      </FieldCard>
+    </>
+  )
+}
---
+++ src/lib/components/Fields/File/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ItemLabel from '../../FormGenrator/ItemLabel'
+import { FormField } from '../../../types/fields'
+import useFieldSet from '../../../hook/useFieldSet'
+import FieldCard from '../../FormGenrator/FieldCard'
+
+export default function FileField({ field, onChange, provided }: { field: FormField; onChange: any; provided: any }) {
+  const { isEdit, handleLabelChange, handleEdit, handleSave, handleCancel, handleDelete, renderData } = useFieldSet(
+    field,
+    onChange,
+  )
+
+  return (
+    <>
+      <FieldCard
+        handleDelete={() => handleDelete(field.id)}
+        handleEdit={handleEdit}
+        handleCancel={handleCancel}
+        handleSave={handleSave}
+        isEdit={isEdit}
+        value={renderData}
+        provided={provided}
+      >
+        <ItemLabel
+          edit={isEdit}
+          value={renderData.label}
+          onChange={handleLabelChange}
+          className="form-control"
+          label="Select File"
+          keyField="label"
+        />
+        FILE OPTIONS
+      </FieldCard>
+    </>
+  )
+}

474e48aea69041373e451e9c535c277aa8c3446d
Smit Vadhiya <smit@topsinfosolutions.com>
2023-08-29 14:39:03 +0530

    singler option change

--- src/lib/components/FormGenrator/Options/SingleOption.tsx
+++ src/lib/components/FormGenrator/Options/SingleOption.tsx
@@ -19,7 +19,9 @@ const SingleOption = ({
   const [isInputMode, setIsInputMode] = useState(false)
   const [value, setValue] = useState(option.label)
   const inputRef = useRef<HTMLInputElement>(null)
-  const handleOnBlur = () => {
+  const handleOnBlur = (e: any) => {
+    console.log('essss', e)
+
     setIsInputMode(false)
     onChange(value, index)
   }
@@ -33,36 +35,39 @@ const SingleOption = ({
   useEffect(() => {
     if (isInputMode && inputRef.current) {
       inputRef.current.focus()
+      inputRef.current.select()
     }
   }, [isInputMode])
 
   return (
-    <div className="d-flex justify-content-between align-items-center">
-      <div>
+    <div className="d-flex justify-content-between align-items-center col-6">
+      <div className="w-100 d-flex justify-content-between align-items-center">
         {icon ? <i className={`fa ${icon} `}></i> : `${index + 1}.`}
         {isInputMode ? (
           <input
             ref={inputRef}
             type="text"
-            className="cursor-pointer mx-2"
+            className="cursor-pointer mx-2 w-100 form-control form-control-1"
             value={value}
             onChange={handleOnChange}
             onBlur={handleOnBlur}
           />
         ) : (
-          <span className="mx-2" onClick={handleOnClick}>
+          <div className="mx-2 w-100" onClick={handleOnClick}>
             {value}
-          </span>
+          </div>
         )}
       </div>
 
-      <i
-        className="mx-5 fa fa-close cursor-pointer"
-        onClick={(e) => {
-          e.stopPropagation()
-          onDelete(option.value)
-        }}
-      ></i>
+      {edit && (
+        <i
+          className="mx-5 fa fa-close cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation()
+            onDelete(option.value)
+          }}
+        ></i>
+      )}
     </div>
   )
 }

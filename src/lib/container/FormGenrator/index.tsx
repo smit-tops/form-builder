6410c7abdfcd3658f579a4c7f8ccaa34b6526712
Smit Vadhiya <smit@topsinfosolutions.com>
2023-08-31 18:48:16 +0530

    remove extra state and upgrade type

--- src/lib/container/FormGenrator/index.tsx
+++ src/lib/container/FormGenrator/index.tsx
@@ -12,11 +12,10 @@ export const FormGenrator = ({
   toolBar,
 }: {
   data?: Array<FormField>
-  onChange?: any
+  onChange?: (data: Array<FormField>) => void
   toolBar?: Array<IToolbarItem>
 }) => {
   const [formData, setFormData] = useState<Array<FormField>>([])
-  const [provider, setProvider] = useState<any>()
 
   useEffect(() => {
     if (data) setFormData(data ?? [])
@@ -25,7 +24,7 @@ export const FormGenrator = ({
   const handleChangeFormData = useCallback(
     (newFormData: Array<FormField>) => {
       setFormData(newFormData)
-      onChange(newFormData)
+      if (onChange) onChange(newFormData)
     },
     [onChange],
   )
@@ -46,7 +45,6 @@ export const FormGenrator = ({
       const newField = { ...field, id: uuid() }
       const newFormData = [...formData]
       newFormData.splice(index + 1, 0, newField)
-      // const newFormData = [...formData, newField}]
       handleChangeFormData(newFormData)
     },
     [formData, handleChangeFormData],
@@ -71,43 +69,17 @@ export const FormGenrator = ({
       setFormData,
       handleChangeFormData,
       toolBar,
-      provider,
-      setProvider,
       handleFieldChange,
       handleRemoveField,
       handleCopyComponent,
     }),
-    [
-      formData,
-      setFormData,
-      handleChangeFormData,
-      toolBar,
-      provider,
-      setProvider,
-      handleFieldChange,
-      handleRemoveField,
-      handleCopyComponent,
-    ],
+    [formData, setFormData, handleChangeFormData, toolBar, handleFieldChange, handleRemoveField, handleCopyComponent],
   )
 
   return (
     <FormGenratorContext.Provider value={value}>
       <div>
         <div className="form-builder">
-          {/* <div className="d-flex align-items-center px-4 py-3 bg-white topbar">
-            <h6 className="fw-bold mb-0">Add Template</h6>
-            <div className="ms-auto">
-              <button type="button" className="btn btn-outline-defult">
-                Cancel
-              </button>
-              <button type="button" className="btn mx-3 btn-outline-darkblue">
-                Preview
-              </button>
-              <button type="button" className="btn btn-darkblue">
-                Save
-              </button>
-            </div>
-          </div> */}
           <div className="d-flex">
             <ToolbarSection />
             <FieldEditSection />
